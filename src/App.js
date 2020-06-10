e7f64c8eaa4d2f15153a3fc11fd5b000fc46cc4a
lomeat <lom3at@gmail.com>
2020-06-10 19:15:16 +0300

    add grid dropdown

--- src/App.js
+++ src/App.js
@@ -1,12 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
+import Grid from "hedron";
 
 export const App = () => {
-  const name = "asdasd";
+  const [isListVisible, setIsListVisible] = useState(false);
 
-  return <Name>Hello, {name}</Name>;
+  const handleClickTitleButton = () => {
+    setIsListVisible((state) => !state);
+  };
+
+  console.log(isListVisible);
+
+  return (
+    <Grid.Provider
+      padding="20px"
+      breakpoints={{ mobile: "-500", desktop: "+501" }}
+    >
+      <List direction="vertical">
+        <Title onClick={handleClickTitleButton}>Title</Title>
+        {isListVisible && (
+          <>
+            <Item>Button 1</Item>
+            <Item>Button 2</Item>
+            <Item>Button 3</Item>
+          </>
+        )}
+      </List>
+    </Grid.Provider>
+  );
 };
 
-const Name = styled.div`
-  font-size: 50px;
+const List = styled(Grid.Bounds)`
+  background: #fff;
+  border: 1px solid black;
+  font-family: sans-serif;
+`;
+
+const Title = styled(Grid.Box)`
+  color: red;
+`;
+
+const Item = styled(Grid.Box)`
+  color: green;
 `;
